a4f9fa796756c6b5c852df782ef095b8aff16f5c
Marcelo Morales <morales.374@osu.edu>
2023-07-24 14:14:36 -0400

    Cleaning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import GeoGebraAPI from './components/GeoGebraAPI';
-import GeogebraApp from './components/GeoGebraAPI';
-
 export default function Home() {
   return (
     <main className='mt-6 flex justify-center items-center'>
